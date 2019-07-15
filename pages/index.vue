39c722ed5d83388b00d03fe41a3dbd9f783da693
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-07-16 00:48:28 +0900

    add: 住人を削除できるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -43,8 +43,19 @@ export default {
       const { data } = await this.$axios.$get('residents')
       this.residents = data
     },
-    deleteResident(residentId) {
-      alert(residentId)
+    async deleteResident(residentId) {
+      await this.$dialog.confirm({
+        title: '住人を削除しますか？',
+        message: 'この操作は取り消すことが出来ません。よろしいですか？',
+        confirmText: '削除する',
+        cancelText: 'キャンセル',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: async () => {
+          await this.$axios.$delete(`residents/${residentId}`)
+          this.initResident()
+        }
+      })
     },
     async createResident(form) {
       await this.$axios.$post('residents', {
